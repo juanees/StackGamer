d77a3b239225624ff629276ddff549864b8e16db
Juan <Juan@JUAN>
2020-11-06 23:18:27 -0300

    more tests

---
+++ Tests/UnitTests/Database/Product.cs
@@ -0,0 +1,25 @@
+﻿using Database;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.UnitTests.Database
+{
+    [TestFixture]
+    public class Product
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void MapDtoToDao_IsWorkingCorrectly()
+        {
+
+        }
+    }
+}
