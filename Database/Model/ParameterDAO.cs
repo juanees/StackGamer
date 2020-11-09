67fce6a0fdf75b726a75ff03ebb435ab71d68bee
Gabriel Rodriguez Castellini <g.rodriguez@assistsa.com.ar>
2020-11-08 21:49:56 -0300

    commit parameter table

---
+++ Database/Model/ParameterDAO.cs
@@ -0,0 +1,22 @@
+﻿using Database.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Database.Model
+{
+    public class ParameterDAO : BaseEntity
+    {
+        [Key]
+        public int ParameterId { get; set; }
+
+        public string Key { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
